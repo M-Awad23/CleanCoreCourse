52692e83e28fe0f3e468e49e03367149b5de857e
Zawad <zawad5954@gmail.com>
2026-09-24 14:01:23 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanCoreCourse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
